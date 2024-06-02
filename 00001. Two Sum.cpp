9078aeb4b8b77d8041cff99444da5ca09caabb23
Alec42Fox <122750551+Alec42Fox@users.noreply.github.com>
2024-06-02 12:29:11 -0400

    Update 00001. Two Sum.cpp

--- 00001. Two Sum.cpp
+++ 00001. Two Sum.cpp
@@ -1,11 +1,8 @@
-#include <unordered_map>
-#include <vector>
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums,const int& target) {
-        unordered_map<int,int> map; //tracks previous indices
-        int index = 0;              //tracks current index
-
+        unordered_map<int,int> map;
+        int index = 0;
         for (int& x : nums) {
             if (map.find(target - x) != map.end()) {
                 return {map[target - x], index};

ac858487716afee93b676630a8425bccdeab54c3
Alec42Fox <122750551+Alec42Fox@users.noreply.github.com>
2024-06-05 16:53:16 -0400

    Update 00067. Add Binary.cpp

--- 00067. Add Binary.cpp
+++ 00067. Add Binary.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    string addString (string a,string& b,int& aa,int& bb) {
+    string addString (string& a,string& b,int& aa,int& bb) {
         int c = 0;
         while (aa>=0) {
-            c += a[aa--]-'0';
-            c += bb>=0 ? b[bb--]-'0' : 0;
+            c += a[aa--]&1;
+            c += bb>=0 ? b[bb--]&1 : 0;
             switch (c) {
                 case 0:
                     cout<<' '<<0;
@@ -30,30 +30,6 @@ public:
             }
             cout<<endl;
         }
-        /*
-        switch (c) {
-            case 0:
-                cout<<' '<<0;
-                break;
-            case 1:
-                cout<<' '<<1;
-                a.insert(0,1,'1');
-                break;
-            case 2:
-                cout<<' '<<2;
-                a.insert(0,1,'0');
-                a.insert(0,1,'1');
-                break;
-            case 3:
-                cout<<' '<<3;
-                a.insert(0,1,'1');
-                a.insert(0,1,'1');
-                break;
-            default:
-                cout<<"ERR"<<endl;
-                return "";
-        }
-        */
         if (c) {a.insert(0,1,'1');}
         return a;
     }

aaa92cf06e63a49b63e7f45d3829f59088600c93
Alec42Fox <122750551+Alec42Fox@users.noreply.github.com>
2024-06-09 23:05:50 -0400

    Update 00003. Longest Substring Without Repeating Characters.cpp
    made for loop iterate backwards instead because funny

--- 00003. Longest Substring Without Repeating Characters.cpp
+++ 00003. Longest Substring Without Repeating Characters.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(const string& s) {
-        std::unordered_map<char,int> map; //store previous char
+        std::vector<int> v(128, INT_MAX); //store previous char
         int int_ans = 0, int_end = s.size();
-        for (int i = int_end-1; i != -1; i--) { //iterate string backwards
-            if (map.find(s[i]) != map.end() && map[s[i]] < int_end) //if char in substring
-                int_end = map[s[i]]; //update end of substring
-            map[s[i]] = i; //add char to map
+        for (int i = int_end-1; i != -1; i--) { //go backwards
+            if (v[s[i]] < int_end) //if char in substring
+                int_end = v[s[i]]; //update end of substring
+            v[s[i]] = i; //add char to map
             int_ans = std::max(int_ans, int_end - i); //update ans
         }
         return int_ans; //return ans

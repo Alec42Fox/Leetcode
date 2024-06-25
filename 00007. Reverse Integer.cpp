3d8206535c0cbec7fa1892b83d1dd0f18fc7057d
Alec42Fox <122750551+Alec42Fox@users.noreply.github.com>
2024-06-25 16:58:48 -0400

    Update 00007. Reverse Integer.cpp

--- 00007. Reverse Integer.cpp
+++ 00007. Reverse Integer.cpp
@@ -1,15 +1,40 @@
+int init = [] {
+    cin.tie(nullptr)->sync_with_stdio(false); //disable sync of I/O to speed it up
+    
+    freopen("user.out", "w", stdout); //redirect output to file "user.out"
+    
+    for (string s; getline(cin, s);) {
+        //get input
+
+        //initialize vars
+        long int_ans = 0;
+        if (s[0] == '-') {
+            for (int int_i = s.size()-1; int_i > 0; --int_i) {
+                int_ans = int_ans*10 + (s[int_i]&15);
+                if (int_ans > 2147483641) {
+                    cout<<'0'<<'\n';
+                    goto goto_jump;
+                }
+            }
+            cout<<'-'<<int_ans<<'\n';
+        } else {
+            for (int int_i = s.size()-1; int_i >= 0; --int_i) {
+                int_ans = int_ans*10 + (s[int_i]&15);
+                if (int_ans > 2147483641) {
+                    cout<<'0'<<'\n';
+                    goto goto_jump;
+                }
+            }
+            cout<<int_ans<<'\n';
+        }
+        goto_jump:
+    }
+    return 0;
+}();
+
 class Solution {
 public:
-    int reverse(int x) {
-        int y=0;
-        int sum=0;
-        while(x!=0){
-            y=x%10;
-            if (sum > INT_MAX / 10 || (sum == INT_MAX / 10 && y > 7)) return 0;
-            if (sum < INT_MIN / 10 || (sum == INT_MIN / 10 && y < -8))return 0;
-            sum=(sum*10)+y;
-            x=x/10;
-        }
-        return int(sum);
+    int reverse(int & x) {
+        return 0;
     }
 };
